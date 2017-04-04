a044005ec815718eba5435cd832e9829d0cbb5a1
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-04 19:54:42 -0300

    Minor refactor in order to have a better separation of concerns

--- server/strategies/facebook.js
+++ server/strategies/facebook.js
@@ -1,18 +1,13 @@
 const Strategy = require('passport-facebook-token');
-const User = require('../models').User;
+const usersController = require('../controllers').users;
 
 module.exports = new Strategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
 },
 (accessToken, refreshToken, profile, cb) => {
-  User.findOrCreate({
-    where: { facebookId: profile.id },
-    defaults: { name: profile.displayName, email: profile.emails[0].value }
-  })
-  .spread((user) => {
-    cb(null, user.get({
-      plain: true
-    }));
-  });
+  usersController.findOrCreate(accessToken, profile)
+    .then((user) => {
+      cb(null, user);
+    });
 });
