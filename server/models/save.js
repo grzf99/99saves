0492de678531ede7c58dd7642084d339e7fc2978
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-04 16:55:21 -0300

    create save model

--- server/models/save.js
+++ server/models/save.js
@@ -1,12 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Save = sequelize.define('Save', {
     title: DataTypes.STRING,
-    description: DataTypes.TEXT('tiny'),
+    description: DataTypes.TEXT,
     image_default: DataTypes.STRING,
     image2: DataTypes.STRING,
     image3: DataTypes.STRING,
-    create_at: DataTypes.DATE,
-    update_at: DataTypes.DATE,
     date_start: DataTypes.DATE,
     date_end: DataTypes.DATE
   });
