3e46b15872e56ed1be3e2f06b81b2eb8ab320c6a
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-05 14:22:29 -0300

    adjust database heroku

--- server/models/index.js
+++ server/models/index.js
@@ -12,10 +12,7 @@ if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
       sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
+      protocol: 'postgres'
     })
   } else {
   sequelize = new Sequelize(
