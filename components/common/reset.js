4eca4596daa97d5dacb9ec732e968103b8784ceb
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-05 22:30:54 -0300

    Adição do CSS Reset e das fontes do projeto

---
+++ components/common/reset.js
@@ -0,0 +1,21 @@
+export default `
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+    min-height: 0;
+    min-width: 0;
+
+    &:before, &:after {
+      box-sizing: border-box;
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
