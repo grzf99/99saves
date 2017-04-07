5c1b395e6f8e613f850bcdb35289113308f3ef5f
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-07 11:07:45 -0300

    add maskinput in input date

--- pages/admin/saves.js
+++ pages/admin/saves.js
@@ -7,7 +7,7 @@ import config from '../../config';
 import Layout from '../../components/admin/layout';
 
 import { Form, Text, Textarea } from 'react-form';
-import DatePicker from 'react-datepicker';
+import MaskedInput from 'react-maskedinput';
 import moment from 'moment';
 import axios from 'axios';
 
@@ -21,7 +21,7 @@ export default class extends React.Component {
     super(props);
     this.state = {
       image_default: '',
-      startDate: moment()
+      startDate: ''
     };
   }
 
@@ -44,10 +44,19 @@ export default class extends React.Component {
     });
   }
 
+  _onChange = (e)  => {
+    var stateChange = {}
+    stateChange[e.target.name] = e.target.value;
+    this.setState(stateChange);
+  }
+
   myForm = (
     <Form
       onSubmit={(values) => {
+        console.log(this.state);
         if(this.state.image_default) values.image_default = this.state.image_default;
+        if(this.state.date_start) values.date_start = moment().format(this.state.date_start, 'mm-dd-yyyy');
+        if(this.state.end) values.date_end = moment().format(this.state.date_end, 'mm-dd-yyyy');
         console.log(values);
         const rest = axios.post(`${config.API_URL}/saves`, values)
         .then(function (response) {
@@ -67,61 +76,59 @@ export default class extends React.Component {
       {({submitForm}) => {
         return (
           <form onSubmit={submitForm}>
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback col-sm-12">
               <label className="control-label">Título</label>
               <div className="controls">
                 <Text field='title' className="form-control"/>
               </div>
             </div>
 
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback col-sm-6">
               <label className="control-label">Data início do save</label>
               <div className="controls">
-                <DatePicker
-                  selected={this.state.startDate}
-                  onChange={this.handleDateChange} />
+                <MaskedInput mask="11/11/1111" className="form-control" name="date_start" placeholder="dd/mm/yyyy" onChange={this._onChange}/>
               </div>
             </div>
 
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback col-sm-6">
               <label className="control-label">Data finalização' do save</label>
               <div className="controls">
                 <div className="controls">
-                  <DatePicker
-                    selected={this.state.startDate}
-                    onChange={this.handleDateChange} />
+                  <MaskedInput mask="11/11/1111" className="form-control" name="date_end" placeholder="dd/mm/yyyy" onChange={this._onChange}/>
                 </div>
               </div>
             </div>
 
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback  col-sm-12">
               <label className="control-label">Description</label>
               <div className="controls">
                 <Textarea field='description' className="form-control"/>
               </div>
             </div>
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback  col-sm-12">
               <label className="control-label">Imagem de destaque</label>
               <div className="controls">
                 <input type='file' name='default_image' className="form-control" onChange={this.handleSave} />
               </div>
             </div>
 
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback  col-sm-12">
               <label className="control-label">Imagem secundária</label>
               <div className="controls">
                 <input type='file' name='imagem2' className="form-control" onChange={this.handleSave} />
               </div>
             </div>
 
-            <div className="form-group has-feedback">
+            <div className="form-group has-feedback col-sm-12">
               <label className="control-label">Imagem secundária</label>
               <div className="controls">
                 <input type='file' name='imagem3' className="form-control" onChange={this.handleSave} />
               </div>
             </div>
             
-            <button type='submit' className="btn btn-default">Salvar</button>
+            <div className="form-group has-feedback col-sm-12">
+              <button type='submit' className="btn btn-default">Salvar</button>
+            </div>
           </form>
         )
       }}
@@ -132,7 +139,7 @@ export default class extends React.Component {
     return (
       <Layout>
         <div className="row">
-          <div class="col-lg-12">
+          <div className="col-lg-12">
             <div className="panel panel-default">
               <div className="panel-heading">
                 <span className="panel-title">Cadastrar Save</span>
