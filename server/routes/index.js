7e07da13facbfeb3d8f631095520584faf62f4c3
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-13 12:03:31 -0300

    add provider routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const savesRouter = require('./saves');
 const usersRouter = require('./users');
 const authRouter = require('./auth');
+const providersRouter = require('./providers');
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.get('/', (req, res) => res.status(200).send({
 router.use('/saves', savesRouter);
 router.use('/users', usersRouter);
 router.use('/auth', authRouter);
+router.use('/providers', providersRouter);
 
 module.exports = router;
