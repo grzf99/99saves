86e47ea9070011e6cbc9b787dabf3fe1ba66cf0f
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-13 18:02:07 -0300

    add crud products

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,13 +1,50 @@
 const Product = require('../models').Product;
+const Save = require('../models').Save;
+const Provider = require('../models').Provider;
 
 module.exports = {
+  list(req, res) {
+    return Product
+      .findAndCountAll({
+        include: [
+          { model: Save },
+          { model: Provider }
+        ],
+        order: [
+          ['id', 'DESC']
+        ],
+        offset: req.query.offset,
+        limit: req.query.limit
+      })
+      .then(resp => res.status(200).send(resp))
+      .catch(error => res.status(400).send(error));
+  },
+
   create(req, res) {
     return Product
-      .create({
-        // SaveId: parseInt(req.params.saveId, 10),
-        // UserId: req.user.id
-      })
-      .then(todo => res.status(201).send(todo))
+      .create(req.body)
+      .then(resp => res.status(201).send(resp))
       .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Product
+      .update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(product => res.status(200).send(product))
+      .catch(error => res.status(400).send(error));
+  },
+
+  delete(req, res) {
+    Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(deletedRecords => res.status(200).json(deletedRecords))
+    .catch(error => res.status(500).json(error));
   }
 };
