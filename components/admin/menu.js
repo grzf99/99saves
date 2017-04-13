abadf8bfedd66b79ab17fee52938e2593a736adb
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-13 14:08:37 -0300

    include provider menu

--- components/admin/menu.js
+++ components/admin/menu.js
@@ -2,13 +2,8 @@ import Link from 'next/link';
 
 export default () => (
   <ul className="nav nav-pills nav-stacked">
-    <li><Link prefetch href='/admin'><a>Dashboard</a></Link></li>
-    <li><Link prefetch href='/admin/saves'><a>Saves</a></Link></li>
-    {/*<li><a href="#/"><i className="glyphicon glyphicon-link" /> Links</a></li>
-    <li><a href="#/"><i className="glyphicon glyphicon-list-alt" /> Reports</a></li>
-    <li><a href="#/"><i className="glyphicon glyphicon-book" /> Books</a></li>
-    <li><a href="#/"><i className="glyphicon glyphicon-briefcase" /> Tools</a></li>
-    <li><a href="#/"><i className="glyphicon glyphicon-time" /> Real-time</a></li>
-    <li><a href="#/"><i className="glyphicon glyphicon-plus" /> Advanced..</a></li>*/}
+    <li><Link prefetch href="/admin"><a>Dashboard</a></Link></li>
+    <li><Link prefetch href="/admin/saves"><a>Saves</a></Link></li>
+    <li><Link prefetch href="/admin/providers"><a>Fornecedores</a></Link></li>
   </ul>
 );
