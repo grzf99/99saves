c15bca7da23414b9d0cc13e25d06e0173162d57f
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-13 16:14:44 -0300

    Alteração dos estilos do toast

--- components/styles/variables.js
+++ components/styles/variables.js
@@ -2,6 +2,7 @@ export const colors = {
   alternateGreen: '#68ce92',
   alternateWhite: '#f1f1f1',
   black: '#1d2028',
+  blue: '#2894e1',
   darkBlue: '#253e4f',
   darkGreen: '#33433c',
   facebookBlue: '#3c5193',
