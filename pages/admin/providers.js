c5a6a6b48dc864c6d05806759a2e4e3bb6a41f8a
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-13 14:08:49 -0300

    create list providers

---
+++ pages/admin/providers.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import 'isomorphic-fetch';
+import Link from 'next/link';
+import axios from 'axios';
+
+import config from '../../config';
+import Layout from '../../components/admin/layout';
+import ListTable from '../../components/admin/list-table-providers';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: []
+    };
+
+    this.refresh = this.refresh.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  componentWillMount() {
+    this.refresh();
+  }
+
+  refresh() {
+    axios.get(`${config.API_URL}/providers`)
+        .then((response) => {
+          this.setState({ ...this.state, list: response.data.rows });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }
+
+  handleDelete(save) {
+    axios.delete(`${config.API_URL}/providers/${save.id}`)
+        .then(() => {
+          this.refresh();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <span className="panel-title">Lista de Fornecedores</span>
+                <Link prefetch href="/admin/saves-create">
+                  <a className="btn btn-xs btn-primary pull-right">Novo</a>
+                </Link>
+              </div>
+
+              <div className="panel-body">
+                <ListTable list={this.state.list} handleDelete={this.handleDelete} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
