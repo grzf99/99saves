cc0a9617010df7f15fd74752f0089e1241dabc97
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-17 16:36:06 -0300

    add link all saves

--- components/toolbar.js
+++ components/toolbar.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { colors } from './styles/variables';
 import Button from './common/button';
 import Container from './common/container';
+import Link from 'next/link';
 
 const Toolbar = styled.header`
   background: ${colors.black};
@@ -18,13 +19,45 @@ const Logo = styled.img`
   margin-left: 5px;
 `;
 
+const MenuLinks = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  min-width: 220px;
+`;
+
+const LinkAllSaves = styled.a`
+  background-color: transparent;
+  color: ${colors.white};
+  cursor: pointer;
+  display: inline-block;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  text-transform: uppercase;
+
+  &:link {
+    color: ${colors.white};
+    transition: .2s ease background-color;
+  }
+
+  &:hover {
+    color: ${colors.alternateGreen};
+  }`;
+
 export default props => (
   <Toolbar>
     <CustomContainer>
       <Logo src="/static/images/logo-99-saves.svg" alt="99saves" />
-      {
-        !props.logged && <Button small outline onClick={props.login}>login</Button>
-      }
+
+      <MenuLinks>
+        <LinkAllSaves href="/saves">
+          todos os saves
+        </LinkAllSaves>
+        {
+          !props.logged && <Button small outline onClick={props.login}>login</Button>
+        }
+      </MenuLinks>
     </CustomContainer>
   </Toolbar>
 );
