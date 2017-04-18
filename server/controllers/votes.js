22447e2cb11abee0f12bd530d9ff0ecdd93dfe6f
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-18 17:28:22 -0300

    Criação do endpoint que retorna o voto do usuário para determinada Subscription

--- server/controllers/votes.js
+++ server/controllers/votes.js
@@ -2,11 +2,27 @@ const Vote = require('../models').Vote;
 const Subscription = require('../models').Subscription;
 
 module.exports = {
+  show(req, res) {
+    return Subscription.findOne({
+      where: {
+        SaveId: req.params.saveId,
+        UserId: req.user ? req.user.id : 1 // TODO: remover esse mock quando a auth estiver OK
+      }
+    })
+    .then(subscription => Vote.findOne({
+      where: {
+        SubscriptionId: parseInt(subscription.id, 10)
+      }
+    }))
+    .then(save => res.status(200).send(save))
+    .catch(error => res.status(400).send(error));
+  },
+
   create(req, res) {
     return Subscription.findOne({
       where: {
         SaveId: req.params.saveId,
-        UserId: req.user.id
+        UserId: req.user ? req.user.id : 1 // TODO: remover esse mock quando a auth estiver OK
       }
     })
     .then(subscription => Vote.create({
--- server/routes/saves.js
+++ server/routes/saves.js
@@ -12,9 +12,12 @@ router.get('/', (req, res, next) => {
   if (req.query.access_token) passport.authenticate('facebook-token')(req, res, next);
   else next();
 }, savesController.list);
-router.post('/:saveId/subscriptions', passport.authenticate('facebook-token'), subscriptionsController.create);
-router.post('/:saveId/votes', passport.authenticate('facebook-token'), votesController.create);
 router.put('/:id', savesController.update);
 router.delete('/:id', savesController.delete);
+
+router.post('/:saveId/subscriptions', passport.authenticate('facebook-token'), subscriptionsController.create);
+// TODO: Adicionar autênticação
+router.get('/:saveId/votes', /* passport.authenticate('facebook-token'), */votesController.show);
+router.post('/:saveId/votes', /* passport.authenticate('facebook-token'), */votesController.create);
 
 module.exports = router;
