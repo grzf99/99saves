5c83946b7b5b5be18b978e6783195b497591e680
Diogo Beda <diogo.fbeda@gmail.com>
2017-04-18 09:41:07 -0300

    Modela Users para autenticação

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    admin: DataTypes.BOOLEAN,
     facebookId: DataTypes.STRING
   }, {
     classMethods: {
