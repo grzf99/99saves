cd8b8ad1c0c259f2fe3703bdec207ac1443fb792
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-24 17:51:54 -0300

    add show products route

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,6 +4,7 @@ const { products } = require('../controllers');
 const router = express.Router();
 
 router.get('/', products.list);
+router.get('/:id', products.show);
 router.post('/', products.create);
 router.put('/:id', products.update);
 router.delete('/:id', products.delete);
