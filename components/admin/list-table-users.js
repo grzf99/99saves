8a476a681c9d512b8ba1f41e3712cb5998a59d86
Ricardo Gonçalves <ricardo@ricardok.com>
2017-04-27 15:48:57 -0300

    add table list users

---
+++ components/admin/list-table-users.js
@@ -0,0 +1,42 @@
+import moment from 'moment';
+import Link from 'next/link';
+
+export default (props) => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map(item => (
+      <tr key={item.id}>
+        <td>{ item.id }</td>
+        <td>{ item.name }</td>
+        <td>{ item.email }</td>
+        <td>{ item.admin ? 'Administrador' : 'Usuário' }</td>
+        <td>
+          <Link prefetch href={`/admin/users-edit?id=${item.id}`}>
+            <a className="">Editar</a>
+          </Link>
+        </td>
+      </tr>
+    ));
+  };
+
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Tipo</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {
+            renderRows()
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
