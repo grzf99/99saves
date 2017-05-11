1f4d22b20c0062e0f5056052e3fdfb01e06f3cee
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-11 17:43:46 -0300

    implemented links and texts in footer

--- components/footer.js
+++ components/footer.js
@@ -42,15 +42,77 @@ const SocialMedia = styled.div`
 `;
 
 const TextLinks = styled.div`
-  align-items: center;
+  align-items: flex-start;
   border-top: 1px solid ${colors.black};
   border-bottom: 1px solid ${colors.black};
   display: flex;
-  justify-content: space-between;
+  font-family: 'Roboto';
+  justify-content: center;
   margin-bottom: 20px;
   max-width: 960px;
   padding: 25px 0;
   width: 100%;
+
+  div:first-child {
+    padding-right: 40px;
+    position: relative;
+
+    &:after {
+      border-right: 1px solid ${colors.black};
+      content: '';
+      height: 100%;
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 1px;
+    }
+  }
+
+  > div:nth-last-child(1) {
+    padding-left: 40px;
+  }
+`;
+
+const FooterTexts = styled.div`
+  color: ${colors.gray};
+  font-family: 'Roboto';
+  font-size: 12px;
+  text-align: right;
+
+  > * {
+    max-width: 300px;
+  }
+`;
+
+const FooterTitle = styled.h3`
+  color: ${colors.white};
+  font-family: Oswald;
+  font-size: 22px;
+  margin-top: 0;
+`;
+
+const FooterText = styled.p`
+  font-size: 12px;
+`;
+
+const FooterLinks = styled.div`
+  text-align: left;
+`;
+
+const FooterLink = styled.div`
+
+  > a:first-child {
+    margin-top: 0;
+  }
+
+  > a {
+    color: ${colors.green};
+    font-family: 'Roboto';
+    float: left;
+    font-size: 14px;
+    margin: 3px;
+    width: 100%;
+  }
 `;
 
 export default props => (
@@ -73,10 +135,24 @@ export default props => (
       </a>
     </SocialMedia>
     <TextLinks>
-      <CustomText white>
-        &reg;
-        2017 | Todos os direitos reservados
-      </CustomText>
+      <FooterTexts>
+        <FooterTitle>99saves.com</FooterTitle>
+        <FooterText>
+          A 1ª plataforma de negociação direta entre consumidores e fabricantes
+        </FooterText>
+        <FooterText>
+          <small>&reg; 2017 | Todos os direitos reservados</small>
+        </FooterText>
+      </FooterTexts>
+
+      <FooterLinks>
+        <FooterTitle>Links</FooterTitle>
+        <FooterLink>
+          <a href="https://medium.com/@99saves">Todos os saves</a>
+          <a href="https://www.facebook.com/99saves/">Perguntas frequentes</a>
+          <a href="https://www.youtube.com">Termos de uso</a>
+        </FooterLink>
+      </FooterLinks>
     </TextLinks>
   </Footer>
 );
