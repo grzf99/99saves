ad1771670813c08f422b2cce2f72fc3f214471e1
Diogo Beda <diogo.fbeda@gmail.com>
2017-05-11 10:13:22 -0300

    Add SignupWelcomeMailer tests

--- server/mailers/signup-welcome.js
+++ server/mailers/signup-welcome.js
@@ -3,12 +3,12 @@ const { sendMail, compileTemplate } = require('../../utils/mailers');
 
 module.exports = {
   async mail(to, context) {
-    const content = await compileTemplate(
-      'mailers/signup-welcome.hbs',
-      context
-    );
     try {
-      await sendMail('Bem-vindo(a) ao 99saves!', to, juice(content));
+      const content = await compileTemplate(
+        'mailers/signup-welcome.hbs',
+        context
+      );
+      return await sendMail('Bem-vindo(a) ao 99saves!', to, juice(content));
     } catch (err) {
       console.log(err);
     }
