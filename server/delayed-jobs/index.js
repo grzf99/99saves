9ece2179c9758162110632625792ac85b1f6281f
Diogo Beda <diogo.fbeda@gmail.com>
2017-05-18 17:14:47 -0300

    Add feedback procesor

--- server/delayed-jobs/index.js
+++ server/delayed-jobs/index.js
@@ -3,6 +3,7 @@ const emailProcessor = require('./email');
 const votationStartProcessor = require('./votation-start');
 const checkoutStartProcessor = require('./checkout-start');
 const lastChanceProcessor = require('./last-chance');
+const feedbackProcessor = require('./feedback');
 
 const options = {
   redis: process.env.NODE_ENV === 'production'
@@ -17,6 +18,7 @@ module.exports = () => {
   queue.process('votation-start', votationStartProcessor);
   queue.process('checkout-start', checkoutStartProcessor);
   queue.process('last-chance', lastChanceProcessor);
+  queue.process('feedback', feedbackProcessor);
 
   global.queue = queue;
 };
