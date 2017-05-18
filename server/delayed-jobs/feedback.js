08373c8e4a69135c8f7a7a1b6fb43eef7e665e92
Diogo Beda <diogo.fbeda@gmail.com>
2017-05-18 17:40:40 -0300

    Add logging

--- server/delayed-jobs/feedback.js
+++ server/delayed-jobs/feedback.js
@@ -20,6 +20,9 @@ module.exports = async (job, done) => {
   if (subscriptions.length === 0) {
     return done();
   }
+  console.log(
+    `running feedback job for save ${save.id} with ${subscriptions.length} subscriptions`
+  );
 
   return Promise.all(
     subscriptions.map((s) => {
