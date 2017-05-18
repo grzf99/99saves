2e3cae037aa9bfe54e9b984a9fdea4a22efb0bd1
Diogo Beda <diogo.fbeda@gmail.com>
2017-05-18 16:54:21 -0300

    Add timezone clock to feedback filter

--- server/models/save.js
+++ server/models/save.js
@@ -164,7 +164,8 @@ module.exports = (sequelize, DataTypes) => {
         feedbackable: {
           where: {
             checkout_end: {
-              $eq: endOfDay(addDays(new Date(), -9))
+              $gt: addHours(startOfDay(addDays(new Date(), -9)), 3),
+              $lt: addHours(endOfDay(addDays(new Date(), -9)), 3)
             }
           }
         }
