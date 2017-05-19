a7ec787725e7e29ea6e221ab319c1e1d6f1b7724
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-19 17:55:31 -0300

    add list users in subscription in save

---
+++ pages/admin/saves-inscritos.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Link from 'next/link';
+
+import withAuth from '../../components/hoc/withAuth';
+import config from '../../config';
+import Layout from '../../components/admin/layout';
+import ListTable from '../../components/admin/list-table-save-users';
+import AlertMessage from '../../components/common/alert-message';
+import RenderIf from '../../components/common/render-if';
+
+class SavesSubscriptions extends React.Component {
+  static getInitialProps({ query }) {
+    return { query };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+      listSave: [],
+      winnerProduct: [],
+      showToast: false,
+      messageToast: '',
+      typeToast: ''
+    };
+
+    this.refresh = this.refresh.bind(this);
+  }
+
+  componentWillMount() {
+    this.refresh();
+    this.getSaves(this.props.query.id);
+  }
+
+  getSaves(id) {
+    this.props.api
+      .get(`/saves/${id}`)
+      .then((response) => {
+        this.setState({
+          ...this.state,
+          listSave: response.data,
+          winnerProduct: response.data.winnerProduct
+        });
+        setTimeout(() => this.setState({ loading: false }), 1500);
+      })
+      .catch((error) => {
+        this.setState({
+          showToast: true,
+          typeToast: 'warning',
+          messageToast: `Problemas ao se comunicar com API: ${error.message}`
+        });
+        setTimeout(() => this.setState({ showToast: false }), 2500);
+      });
+  }
+
+  refresh() {
+    this.props.api
+      .get(`/saves/${this.props.query.id}/users`)
+      .then((response) => {
+        this.setState({ ...this.state, list: response.data.subscriptions });
+      })
+      .catch((error) => {
+        this.setState({
+          showToast: true,
+          typeToast: 'warning',
+          messageToast: `Problemas ao se comunicar com api: ${error.message}`
+        });
+        setTimeout(() => this.setState({ showToast: false }), 2500);
+      });
+  }
+
+
+  render() {
+    return (
+      <Layout>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <RenderIf expr={this.state.listSave.checkoutOpen || this.state.listSave.finished}>
+                  <span className="panel-title">Lista de Inscritos no save - (<b>{this.state.listSave.title}</b>)</span>
+                </RenderIf>
+              </div>
+
+              <div className="panel-body">
+                <RenderIf expr={this.state.listSave.checkoutOpen || this.state.listSave.finished}>
+                  <div>
+                    <div className="row">
+                      <div className="col-lg-12">
+                        <img src={this.state.listSave.image_default} width="80"/>
+                        <span className="panel-title">  Produto Vencedor - <b>{this.state.winnerProduct.title}</b></span>
+                        <span/>
+                      </div>
+                    </div>
+                    <div className="form-group"/>
+                  </div>
+                </RenderIf>
+                <RenderIf expr={this.state.listSave.checkoutOpen || this.state.listSave.finished}>
+                  <ListTable
+                    list={this.state.list}
+                  />
+                </RenderIf>
+              </div>
+            </div>
+          </div>
+        </div>
+        <AlertMessage type={this.state.typeToast} show={this.state.showToast}>
+          {this.state.messageToast}
+        </AlertMessage>
+      </Layout>
+    );
+  }
+}
+
+export default withAuth({ isAdminPage: true })(SavesSubscriptions);
