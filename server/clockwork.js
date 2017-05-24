41e000a92fd1db324cae2d172a655870b9297770
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-24 09:51:41 -0300

    add new schedules

--- server/clockwork.js
+++ server/clockwork.js
@@ -5,7 +5,25 @@ const LastChanceMailer = require('./mailers/last-chance');
 const FeedbackMailer = require('./mailers/feedback');
 
 module.exports = () => {
-  schedule.scheduleJob('20 1 * * *', () => {
+  schedule.scheduleJob('* 13 * * *', () => {
+    VotationStartMailer.verify();
+  });
+  schedule.scheduleJob('1 13 * * *', () => {
+    CheckoutStartMailer.verify();
+  });
+  schedule.scheduleJob('2 13 * * *', () => {
+    LastChanceMailer.verify();
+  });
+  schedule.scheduleJob('3 13 * * *', () => {
+    FeedbackMailer.verify();
+  });
+  schedule.scheduleJob('* 15 * * *', () => {
+    VotationStartMailer.verify();
+    CheckoutStartMailer.verify();
+    LastChanceMailer.verify();
+    FeedbackMailer.verify();
+  });
+  schedule.scheduleJob('30 17 * * *', () => {
     VotationStartMailer.verify();
     CheckoutStartMailer.verify();
     LastChanceMailer.verify();
