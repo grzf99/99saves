525d952ce41361bb76c61813467fbd2a13cea234
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-25 10:10:03 -0300

    adjust clockwork

--- server/clockwork.js
+++ server/clockwork.js
@@ -5,25 +5,25 @@ const LastChanceMailer = require('./mailers/last-chance');
 const FeedbackMailer = require('./mailers/feedback');
 
 module.exports = () => {
-  // schedule.scheduleJob('1 18 * * *', () => {
-  //   VotationStartMailer.verify();
-  // });
-  // schedule.scheduleJob('2 18 * * *', () => {
-  //   CheckoutStartMailer.verify();
-  // });
-  // schedule.scheduleJob('3 18 * * *', () => {
-  //   LastChanceMailer.verify();
-  // });
-  // schedule.scheduleJob('4 18 * * *', () => {
-  //   FeedbackMailer.verify();
-  // });
-  // schedule.scheduleJob('* 15 * * *', () => {
-  //   VotationStartMailer.verify();
-  //   CheckoutStartMailer.verify();
-  //   LastChanceMailer.verify();
-  //   FeedbackMailer.verify();
-  // });
-  schedule.scheduleJob('47 23 * * *', () => {
+  schedule.scheduleJob('1 18 * * *', () => {
+    VotationStartMailer.verify();
+  });
+  schedule.scheduleJob('2 18 * * *', () => {
+    CheckoutStartMailer.verify();
+  });
+  schedule.scheduleJob('3 18 * * *', () => {
+    LastChanceMailer.verify();
+  });
+  schedule.scheduleJob('4 18 * * *', () => {
+    FeedbackMailer.verify();
+  });
+  schedule.scheduleJob('* 15 * * *', () => {
+    VotationStartMailer.verify();
+    CheckoutStartMailer.verify();
+    LastChanceMailer.verify();
+    FeedbackMailer.verify();
+  });
+  schedule.scheduleJob('30 17 * * *', () => {
     VotationStartMailer.verify();
     CheckoutStartMailer.verify();
     LastChanceMailer.verify();
