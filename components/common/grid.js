5963fa5dacf48e1c1dcbdad58387c9a55183d13e
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-25 01:36:00 -0300

    adjust grid in mobile

--- components/common/grid.js
+++ components/common/grid.js
@@ -4,10 +4,20 @@ export const Row = styled.section`
   display: flex;
   flex-flow: row;
   flex-wrap: wrap;
+
+  @media (max-width: 480px) {
+    position: relative;
+    display: inline-table;
+  }
 `;
 
 export const Column = styled.div`
   flex: ${props => props.third ? '0 0 33%' : '1'};
+  @media (max-width: 480px) {
+    position: ${props => props.third ? 'absolute' : 'relative'};
+    top: ${props => props.third ? '0' : '320px'};
+    margin: 0 10px;
+  }
 `;
 
 export default {
