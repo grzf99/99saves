f548065e6d8b68b6274bade9a1e318f51a94af7c
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-25 14:53:12 -0300

    adjut modal coupon

--- components/common/checkout-modal.js
+++ components/common/checkout-modal.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Modal from '../common/modal';
 import CheckoutContent from './checkout-content';
 
-const defaultWidth = "88%";
-
 export default ({ submitText, ...cleanedProps, width, save }) => (
-  <Modal {...cleanedProps} width={ width ? width : defaultWidth }>
+  <Modal {...cleanedProps} width="88%">
     <CheckoutContent save={ save } />
   </Modal>
 );
