3d6f3019c5a41fd98f4558a78172a5100f8be5fa
Ricardo Gonçalves <ricardo@ricardok.com>
2017-05-25 19:28:05 -0300

    adjust clockwork 23PM

--- server/clockwork.js
+++ server/clockwork.js
@@ -5,7 +5,7 @@ const LastChanceMailer = require('./mailers/last-chance');
 const FeedbackMailer = require('./mailers/feedback');
 
 module.exports = () => {
-  schedule.scheduleJob('0 22 * * *', () => {
+  schedule.scheduleJob('0 23 * * *', () => {
     VotationStartMailer.verify();
     CheckoutStartMailer.verify();
     LastChanceMailer.verify();
