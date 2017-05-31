fa0461dc38d0f38016940b14ecbd42b2abb2ec0b
AdrianoMoura <amourapflorentino@gmail.com>
2017-05-31 18:00:20 -0300

    include "inscritos" link on every save table item

--- components/admin/list-table-saves.js
+++ components/admin/list-table-saves.js
@@ -39,7 +39,6 @@ export default (props) => {
             : ''}
         </td>
         <td>
-          {(item.checkoutOpen || item.finished) ?
             <Link prefetch href={`/admin/saves-inscritos?id=${item.id}`}>
               <a className="">Inscritos</a>
             </Link>
