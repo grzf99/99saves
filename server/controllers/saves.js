14aa803a69b11606bc4025569865625350753108
AdrianoMoura <amourapflorentino@gmail.com>
2017-06-11 23:21:45 -0300

    fix saves controller list

--- server/controllers/saves.js
+++ server/controllers/saves.js
@@ -182,6 +182,7 @@ function createListQuery(req) {
             required: !!(req.query.filters &&
               req.query.filters.subscribed === 'true')
           }
+        ]
 
         query.where = {};
       }
