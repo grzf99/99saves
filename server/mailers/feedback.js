df383cd503e4d6afde5672facbff24bf5ab0b50c
AdrianoMoura <amourapflorentino@gmail.com>
2017-06-26 16:30:40 -0300

    fix lastchance and feedback mail

--- server/mailers/feedback.js
+++ server/mailers/feedback.js
@@ -3,7 +3,7 @@ const { Save, Product } = require('../models');
 module.exports = {
   async verify() {
     const saves = await Save.scope('feedbackable').findAll({
-      include: [{ model: Product, required: true }]
+      include: [{ model: Product, required: true, include: [Vote, Provider] }]
     });
     return saves.map(save =>
       global.queue.create('feedback', { save }).removeOnComplete(true).save()
