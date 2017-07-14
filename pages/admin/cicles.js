dd3c29a502a60f2becd1a5fa8811f44dd65775f7
AdrianoMoura <amourapflorentino@gmail.com>
2017-07-14 18:28:41 -0300

    debug admin

--- pages/admin/cicles.js
+++ pages/admin/cicles.js
@@ -30,6 +30,7 @@ class Cicles extends React.Component {
     this.props.api
       .get('/cicles/all')
       .then((response) => {
+        console.log(response);
         this.setState({ ...this.state, list: response.data });
       })
       .catch((error) => {
