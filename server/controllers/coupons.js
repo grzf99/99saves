abf5d53e3ccc0c91b46e7800a9538236833d4d03
AdrianoMoura <amourapflorentino@gmail.com>
2017-07-20 19:02:11 -0300

    remove coupons without relationship

--- server/controllers/coupons.js
+++ server/controllers/coupons.js
@@ -1,4 +1,13 @@
 const { Coupon, Subscription, User, Save, Product } = require('../models');
+const {
+  startOfDay,
+  endOfDay,
+  addDays,
+  isBefore,
+  isAfter,
+  isSameDay,
+  addHours
+} = require('date-fns');
 
 module.exports = {
   list(req, res) {
@@ -47,5 +56,58 @@ module.exports = {
     })
       .then(() => res.sendStatus(200))
       .catch(error => res.status(500).json(error));
+  },
+
+  deleteOld(req, res) {
+    return Save.findAndCountAll(
+          {
+          where: {
+            date_end: { $lt: addDays(new Date(), -10) }
+          }
+        }
+      )
+      .then(({ rows }) => {
+        const saves = rows.map(save => save.id);
+
+        return Subscription.findAndCountAll({
+          where: {
+            SaveId: saves
+          }
+        })
+          .then(({ rows }) => {
+            const subs = rows.map(sub => sub.id);
+
+            return Coupon.findAndCountAll({
+              where: {
+                SubscriptionId: { $notIn: subs }
+              }
+            })
+              .then(({ rows }) => {
+                const coups = rows.map(coup => coup.id);
+
+                return Coupon.destroy({
+                  where: {
+                    id: coups
+                  }
+                })
+                  .then(() => res.sendStatus(200))
+                  .catch(error => res.status(500).json(error));
+              })
+              .catch(error => res.status(400).send(error));
+
+          })
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+
+  },
+  deleteWithNoRelationship(req, res) {
+    return  Coupon.destroy({
+        where: {
+          SubscriptionId: null
+        }
+      })
+        .then(() => res.sendStatus(200))
+        .catch(error => res.status(500).json(error));
   }
 };
--- server/routes/coupons.js
+++ server/routes/coupons.js
@@ -7,6 +7,8 @@ const router = express.Router();
 router.get('/', adminAuthentication(), coupons.list);
 router.get('/:id', adminAuthentication(), coupons.show);
 router.put('/:id', adminAuthentication(), coupons.update);
+router.delete('/deleteWithNoRelationship', adminAuthentication(), coupons.deleteWithNoRelationship);
+router.delete('/deleteOld', adminAuthentication(), coupons.deleteOld);
 router.delete('/:id', adminAuthentication(), coupons.delete);
 
 module.exports = router;
