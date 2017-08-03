06fab094694eb7598841416ba68d9ff36338a65f
AdrianoMoura <amourapflorentino@gmail.com>
2017-08-03 18:49:47 -0300

    provider

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/login', auth.clientLogin);
 router.post('/login/admin', auth.adminLogin);
-router.post('/login/provider', auth.providerAdmin);
+router.post('/login/provider', auth.providerLogin);
 router.post('/forgot-password', auth.forgotPassword);
 router.post('/reset-password', auth.resetPassword);
 
