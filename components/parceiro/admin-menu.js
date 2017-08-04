21ff25db4251ada17062e05cd3e6c80d66893000
AdrianoMoura <amourapflorentino@gmail.com>
2017-08-03 21:15:25 -0300

    provider user menu and logout

---
+++ components/parceiro/admin-menu.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import withAuth from '../hoc/withApi';
+import moment from 'moment';
+
+class AdminMenu extends Component {
+
+  constructor() {
+    moment.lang('pt-BR');
+  }
+
+  render() {
+    return (
+      <li className="dropdown user user-menu">
+
+        <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+
+          <img src="/static/assets_admin/img/user2-160x160.jpg" className="user-image" alt="User Image"/>
+
+          <span className="hidden-xs">{this.props.currentUser.name}</span>
+        </a>
+        <ul className="dropdown-menu">
+
+          <li className="user-header">
+            <img src="/static/assets_admin/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
+
+            <p>
+              {this.props.currentUser}
+              <small>Usuário desde {moment(this.props.currentUser.createdAt).format('MMM YY')}</small>
+            </p>
+          </li>
+
+          <li className="user-body">
+            <div className="row">
+              <div className="col-xs-4 text-center">
+                <a href="#">Followers</a>
+              </div>
+              <div className="col-xs-4 text-center">
+                <a href="#">Sales</a>
+              </div>
+              <div className="col-xs-4 text-center">
+                <a href="#">Friends</a>
+              </div>
+            </div>
+
+          </li>
+
+          <li className="user-footer">
+            <div className="pull-left">
+              <a href="#" className="btn btn-default btn-flat">Profile</a>
+            </div>
+            <div className="pull-right">
+              <a href="/parceiro" className="btn btn-default btn-flat" onClick={this.props.onLogout}>Sair</a>
+            </div>
+          </li>
+        </ul>
+      </li>
+    );
+  }
+}
+
+export default withAuth()(AdminMenu);
--- components/parceiro/custom_nav.js
+++ components/parceiro/custom_nav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import AdminMenu from './admin-menu';
 
 class CustomNav extends Component {
   render() {
@@ -111,50 +112,7 @@ class CustomNav extends Component {
             </ul>
           </li>
 
-          <li className="dropdown user user-menu">
-
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-
-              <img src="/static/assets_admin/img/user2-160x160.jpg" className="user-image" alt="User Image"/>
-
-              <span className="hidden-xs">{this.props.currentUser.name}</span>
-            </a>
-            <ul className="dropdown-menu">
-
-              <li className="user-header">
-                <img src="/static/assets_admin/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
-
-                <p>
-                  Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
-                </p>
-              </li>
-
-              <li className="user-body">
-                <div className="row">
-                  <div className="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div className="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div className="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-
-              </li>
-
-              <li className="user-footer">
-                <div className="pull-left">
-                  <a href="#" className="btn btn-default btn-flat">Profile</a>
-                </div>
-                <div className="pull-right">
-                  <a href="#" className="btn btn-default btn-flat">Sair</a>
-                </div>
-              </li>
-            </ul>
-          </li>
+          <AdminMenu currentUser={this.props.currentUser}/>
 
           <li>
             <a href="#" data-toggle="control-sidebar">
