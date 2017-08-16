56530bde5bb23d1202e0f75f52a07867957f1925
AdrianoMoura <amourapflorentino@gmail.com>
2017-08-16 18:48:41 -0300

    Categories list master

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -9,7 +9,11 @@ module.exports = {
   },
 
   list(req, res) {
-    return Category.findAndCountAll()
+    return Category.findAndCountAll({
+      include: [{
+        model: Category
+      }]
+    })
       .then(({ rows }) => {
         const categories = rows.map(category => category.toJSON());
         res.status(200).send(categories);
